b49cd38f08cb0d8254945ad204b4c56ba7915bb8
A-Abbaszadeh <abbas.abbaszadeh@hotmail.com>
2022-07-08 17:57:43 +0430

    Rollback

--- MicroserviceShop.ApiGateway/Startup.cs
+++ MicroserviceShop.ApiGateway/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -29,6 +30,10 @@ namespace MicroserviceShop.ApiGateway
         {
             services.AddControllers();
             services.AddOcelot();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "MicroserviceShop.ApiGateway", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,6 +42,8 @@ namespace MicroserviceShop.ApiGateway
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MicroserviceShop.ApiGateway v1"));
             }
 
             app.UseHttpsRedirection();
