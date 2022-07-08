afe455fad494a550c091e5246fbb535204e94ee3
A-Abbaszadeh <abbas.abbaszadeh@hotmail.com>
2022-07-08 14:03:33 +0430

    Add Product Entity

---
+++ Microservices/MicroserviceShop.ProductService/Domain/Product.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MicroserviceShop.ProductService.Domain
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Guid? CategoryId { get; set; }
+    }
+}
