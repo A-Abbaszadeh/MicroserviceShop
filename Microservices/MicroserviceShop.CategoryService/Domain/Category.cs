72754a037021707a10ec7282b773f7151cbeaa4c
A-Abbaszadeh <abbas.abbaszadeh@hotmail.com>
2022-07-08 15:01:08 +0430

    Add Category Entity

---
+++ Microservices/MicroserviceShop.CategoryService/Domain/Category.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace MicroserviceShop.CategoryService.Domain
+{
+    public class Category
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
