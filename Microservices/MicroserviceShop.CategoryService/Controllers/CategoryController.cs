abc31cb33222eff5cce3c195165102a21b133860
A-Abbaszadeh <abbas.abbaszadeh@hotmail.com>
2022-07-08 15:16:33 +0430

    Add CategoryController.

---
+++ Microservices/MicroserviceShop.CategoryService/Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using MicroserviceShop.CategoryService.Application;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MicroserviceShop.CategoryService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryServices _categoryServices;
+
+        public CategoryController(ICategoryServices categoryServices)
+        {
+            _categoryServices = categoryServices;
+        }
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(_categoryServices.GetAll());
+        }
+    }
+}
