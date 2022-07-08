6040f13dacc533bfbff56586b7f228493490c69d
A-Abbaszadeh <abbas.abbaszadeh@hotmail.com>
2022-07-08 14:58:08 +0430

    Add ProductController.

---
+++ Microservices/MicroserviceShop.ProductService/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using MicroserviceShop.ProductService.Application;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MicroserviceShop.ProductService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductServices _productServices;
+
+        public ProductController(IProductServices productServices)
+        {
+            _productServices = productServices;
+        }
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(_productServices.GetAll());
+        }
+    }
+}
