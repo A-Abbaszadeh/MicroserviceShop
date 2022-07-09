0606f95cedeaf6818ee2933f964957a71f5b9c16
A-Abbaszadeh <abbas.abbaszadeh@hotmail.com>
2022-07-10 02:16:46 +0430

    Create AccountController with Authenticate method

---
+++ Microservices/MicroserviceShop.IdentityService/Controllers/AccountController.cs
@@ -0,0 +1,35 @@
+﻿using MicroserviceShop.IdentityService.Application;
+using MicroserviceShop.IdentityService.Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MicroserviceShop.IdentityService.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IJwtAuthenticationService _jwtAuthenticationService;
+
+        public AccountController(IJwtAuthenticationService jwtAuthenticationService)
+        {
+            _jwtAuthenticationService = jwtAuthenticationService;
+        }
+
+        [HttpGet("index")]
+        public IActionResult Index()
+        {
+            return Ok("This is for test");
+        }
+
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate([FromBody] User user)
+        {
+            var jwtToken = _jwtAuthenticationService.Authenticate(user.Username, user.Password);
+
+            if (jwtToken is null) return Unauthorized();
+
+            return Ok(jwtToken);
+        }
+
+
+    }
+}
