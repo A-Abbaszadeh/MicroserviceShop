9f8586699c7b044755edca548758cfe4982ffb79
A-Abbaszadeh <abbas.abbaszadeh@hotmail.com>
2022-07-10 00:12:20 +0430

    Create UserRepository.

--- Microservices/MicroserviceShop.IdentityService/Startup.cs
+++ Microservices/MicroserviceShop.IdentityService/Startup.cs
@@ -1,3 +1,4 @@
+using MicroserviceShop.IdentityService.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,12 +27,13 @@ namespace MicroserviceShop.IdentityService
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "MicroserviceShop.IdentityService", Version = "v1" });
             });
+
+            services.AddTransient<IUserRepository, UserRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
